e3c74204bc9f89c29f89dcebff1037991b93ef30
Olegeg <bgpfnic@protonmail.com>
2022-03-23 23:44:47 +0300

    show symbol in window

--- js/app.js
+++ js/app.js
@@ -9,10 +9,14 @@ boxCanvas.append(canvas);
 
 //получение переменных из css
 const root = getComputedStyle(document.querySelector(":root"));
-//console.log(root.getPropertyValue("--white-color"));
 
 // Main Game
 const listWords = ["apple", "lock", "book", "changer"];
+const wordsList = document.querySelector(".words-list");
+const output = document.querySelector(".output");
+
+let outputBox;
+let secretWord;
 
 // Paint Gallows
 const paintCoords = [
@@ -29,14 +33,23 @@ const paintCoords = [
     {moveX: canvas.width - 50, moveY: 170, x: canvas.width - 30, y: 190},
 ];
 
-function paintSceletor() {
+function paintSceleton(count) {
+    if (count-1 > 10) {
+        gameOver();
+        return;
+    }
     ctx.fillStyle = root.getPropertyValue("--white-color");
     ctx.strokeStyle = root.getPropertyValue("--white-color");
     ctx.lineWidth = 2;
+    
     ctx.beginPath();
-    for (let item of paintCoords) {
-        if (item.arc) paintArc(item.moveX, item.moveY, item.x, item.y);
-        else paintLine(item.moveX, item.moveY, item.x, item.y);
+    let i = 0;
+    while (count > i) {
+        const {moveX, moveY, x, y, arc} = paintCoords[i];
+
+        if (arc) paintArc(moveX, moveY, x, y);
+        else paintLine(moveX, moveY, x, y);
+        i++;
     }
     ctx.stroke();
 }
@@ -51,4 +64,56 @@ function paintArc(moveX, moveY, x, y) {
     ctx.arc(x, y, 10, 30, Math.PI * 2, true);
 }
 
-paintSceletor();
+
+function gameOver() {
+
+}
+
+function randomWord(arr) {
+    const rand = Math.floor(Math.random() * arr.length);
+    return arr[rand];
+}
+
+function selectWord() {
+    const secretWord = randomWord(listWords).split("");
+    
+    const divBox = document.createElement("div");
+    divBox.className = "output-box";
+    secretWord.forEach(item => {
+        let divItem = document.createElement("div");
+        divItem.className = "output-item";
+        divBox.append(divItem);
+    })
+
+    output.append(divBox);
+    outputBox = divBox;
+
+    return secretWord;
+}
+
+let activeSymbol = [];
+wordsList.addEventListener("click", event => {
+    const {target} = event;
+    if (!target.closest(".words-item")) return;
+
+    const symbol = target.innerHTML.toLowerCase();
+
+    if (activeSymbol.includes(symbol)) return;
+    activeSymbol.push(symbol);
+    
+    if (secretWord.includes(symbol)) {
+        let pos = 0;
+        while (~(pos = secretWord.indexOf(symbol, pos))) {
+            outputBox.children[pos].innerHTML = symbol;
+            pos++;
+        }
+    }
+    target.classList.add("words-item_active");
+});
+
+function gameLoop() {
+    secretWord = selectWord();
+    console.log(secretWord);
+}
+
+gameLoop();
